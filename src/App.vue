c5186cb2e5ed34ee9a5de087f22f3f67ef21c3e2
Disant Upadhyay <disantupadhyay07@gmail.com>
2021-04-03 14:41:50 -0230

    Classical Music App

---
+++ src/App.vue
@@ -0,0 +1,225 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Classical Music</h1>
+    </header>
+    <main>
+      
+      <section class="player">
+        <h2 class="song-title">{{current.title}} - <span>{{current.artist}}</span></h2>
+      </section>
+      
+      <div class="control">
+        <button class="prev" @click="prev">Prev</button>
+        <button class="play" v-if="!isPlaying" @click="play">Play</button>
+        <button class="pause" v-else @click="pause">Pause</button>
+        <button class="next" @click="next">Next</button>
+      </div>
+      
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" 
+        :class="(song.src === current.src) ? 'song-playing' : 'song'">{{song.title}} - {{song.artist}}</button>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+data () {
+    return{
+      current:{},
+      index: 0,
+      isPlaying: false,
+      songs:[
+        {
+          title: 'Symphony 7-2 (Liszt-Piano)',
+          artist: 'Beethoven',
+          src: require('./assets/beethoven-symphony7-2-liszt-piano.mp3')
+        },
+        {
+          title: 'Salut-d-amour (Violin-Piano)',
+          artist: 'Edward Elgar',
+          src: require('./assets/edward-elgar-salut-d-amour-violin-piano.mp3')
+        },
+        {
+          title: 'Symphony41-3 (Flute-Piano)',
+          artist: 'Mozart',
+          src: require('./assets/mozart-symphony41-3-flute-piano.mp3')
+        },
+      ],
+      player: new Audio(),
+    }
+  },
+  methods: {
+    play(song){
+      if(typeof song.src != 'undefined'){
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.player.addEventListener('ended', function(){//to play the next song after current song has ended
+        this.index++;
+
+        if(this.index > this.songs.length - 1){
+        this.index = 0;
+        }
+        this.current = this.songs[this.index];
+
+        this.play(this.current);
+
+        }.bind(this))
+        this.isPlaying = true;
+    },
+    pause(){
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    prev(){
+      this.index--;
+       if(this.index < 0){
+        this.index = this.songs.length-1;
+      }
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
+    
+    },
+    next(){
+      this.index++;
+      if(this.index > this.songs.length - 1){
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
+    },
+  },
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+    // this.player.play();
+  },
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  /* background: url("./assets/pic.jpg");
+  background-repeat: no-repeat;
+  background-size: cover; */
+ 
+}
+body{
+  font-family: sans-serif;
+}
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #855e42;
+  color: #fff;
+}
+main{
+  width: 100%;
+  margin: 0 auto;
+  padding: 25px;
+}
+.song-title{
+  color: #53565a;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center; 
+}
+.song-title span{
+  font-weight: 400;
+  font-style: italic;
+}
+.control{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+button{
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+}
+.play{
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 40px;
+  margin: 0 15px;
+  border-radius: 8px;
+  color: #fff;
+  background-color: #f29c33;
+}
+.pause{
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 31.5px;
+  margin: 0 14.5px;
+  border-radius: 8px;
+  color: #fff;
+  background-color: #f29c33;
+}
+.next, .prev{
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0 15px;
+  border-radius: 6px;
+  color: #fff;
+  background-color: #f7cf1e;
+}
+.playlist{
+  padding: 0 400px;
+}
+.playlist h3{
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+.playlist .song{
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.playlist .song:hover{
+  color: #f7cf1e;
+}
+.song-playing{
+  display: block;
+  width: 100%;
+  padding: 15px 0;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+  color: #fff;
+  background-image: linear-gradient(to right, #f29c33,#f7cf1e );
+}
+</style>
